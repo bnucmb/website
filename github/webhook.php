73325a5f9c4ead3f1605bb2dba33d9b4c55da2b2
Lei Zhao <leizhao4@gmail.com>
2013-09-13 10:08:35 +0800

    New way to handle webhooks

--- github/webhook.php
+++ github/webhook.php
@@ -4,7 +4,6 @@
 // Written by Lei Zhao on 09/13/13
  
 if ($_POST["payload"]) {
-  `/home/cmb/git/website-dev.git/hooks/github-webhook 2>&1`;
-  `/home/cmb/git/website-prod.git/hooks/github-webhook 2>&1`;
+  `touch ../../git/webhook/updated`;
 }
  
