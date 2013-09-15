68d6b96828569c8f2517199371b6b1e17767dbdd
Lei Zhao <leizhao4@gmail.com>
2013-09-14 21:55:39 -0500

    Avoid undefined index warnings

--- github/webhook.php
+++ github/webhook.php
@@ -3,7 +3,7 @@
 // Auto deploy website after github receives a push
 // Written by Lei Zhao on 09/13/13
  
-if ($_POST["payload"]) {
+if (isset($_POST["payload"])) {
   `touch ../../git/webhook/updated`;
 }
  
